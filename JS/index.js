ad45b15660a5739fc56e860b2b5a54e46f107df7
AllenMoringa <allen.mathews@student.moringaschool.com>
2022-07-17 12:32:55 +0300

    JS first function

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+function totalProduction(){
+    console.log('Your production in Shed A 510 litres per day')
+    console.log('Your production in Shed B 308 litres per day')
+    console.log('Your production in Shed C 486 litres per day')
+    console.log('Your production in Shed D 308 litres per day')
+    console.log('The total production is 1876 litres per day')
+}
+totalProduction()
