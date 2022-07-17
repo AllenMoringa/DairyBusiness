9fd0165af8683bb080eaaf11d4ae5df09dd63858
AllenMoringa <allen.mathews@student.moringaschool.com>
2022-07-17 23:50:56 +0300

    js

--- JS/index.js
+++ JS/index.js
@@ -45,6 +45,42 @@ const answerD = parseInt(inputD.value);
        incomeOverTime(answer5, 7);
        incomeOverTimeYear(answer5, 365);
        
+       let January = answer5 * 45 * 31;
+       document.getElementById('results8').innerHTML= `Your income for January is ${January}`
+
+       let Feb = answer5 * 45 * 28;
+       document.getElementById('results9').innerHTML= `Your income for February is ${Feb}`
+
+       let March = answer5 * 45 * 31;
+       document.getElementById('results10').innerHTML= `Your income for March is ${March}`
+
+       let April = answer5 * 45 * 30;
+       document.getElementById('results11').innerHTML= `Your income for April is ${April}`
+
+       let May = answer5 * 45 * 31;
+       document.getElementById('results12').innerHTML= `Your income for May is ${May}`
+
+       let June = answer5 * 45 * 30;
+       document.getElementById('results13').innerHTML= `Your income for June is ${June}`
+
+       let July = answer5 * 45 * 31;
+       document.getElementById('results14').innerHTML= `Your income for July is ${July}`
+
+       let August = answer5 * 45 * 31;
+       document.getElementById('results15').innerHTML= `Your income for August is ${August}`
+
+       let September = answer5 * 45 * 30;
+       document.getElementById('results16').innerHTML= `Your income for September is ${September}`
+
+       let October = answer5 * 45 * 31;
+       document.getElementById('results17').innerHTML= `Your income for October is ${October}`
+
+       let November = answer5 * 45 * 30;
+       document.getElementById('results18').innerHTML= `Your income for November is ${November}`
+
+       let December = answer5 * 45 * 31;
+       document.getElementById('results19').innerHTML= `Your income for December is ${December}`
+
 
 })
 
@@ -61,3 +97,4 @@ function incomeOverTimeYear(totalProduction, time){
 weeklyIncomeP1.innerHTML = `Your weekly income will be Ksh ${yearlyIncome}`
 
 }
+
